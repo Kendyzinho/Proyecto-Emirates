1f1f9baea5be774b8eefc91f72b91c7e2822b7b8
Kendyzinho <rafakendy2003@gmail.com>
2025-11-18 23:07:23 -0300

    Router
    Se agrega las rutas de login y registro.

--- emirates/src/app/app-routing.module.ts
+++ emirates/src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'header', component: HeaderComponent},
-  {path: 'footer', component: FooterComponent}
+  {path: 'footer', component: FooterComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
