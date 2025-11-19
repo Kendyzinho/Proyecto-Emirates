be1f5b292e4e140f1d3c64e30ca90309338a1a1e
Kendyzinho <rafakendy2003@gmail.com>
2025-11-18 22:46:10 -0300

    Header + Footer
    Junción de header y footer al home.

--- emirates/src/app/app-routing.module.ts
+++ emirates/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'home', component: HomeComponent},
-  {path: 'header', component: HeaderComponent}
+  {path: 'header', component: HeaderComponent},
+  {path: 'footer', component: FooterComponent}
 ];
 
 @NgModule({
